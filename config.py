f3f0b7ab1110c8afa5885ac3b113017331144553
Ataraxia-XD <107171831+Mufid456@users.noreply.github.com>
2023-10-27 20:13:49 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "6401549788:AAEMbArh2PMITHcmLpXkB9ivpDLXMfNbE7Q") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "@gstmfd") # jangan dirubah agar tidak error
+GROUP = os.environ.get("GROUP", "") # 
+CHANNEL = os.environ.get("CHANNEL", "Pavrdd") # 
